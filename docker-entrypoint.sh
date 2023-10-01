627efeaa2882a42f2d460a093d3ddede29737d9e
Jim Thoburn <jim@tobbi.co>
2023-10-01 08:14:20 -0700

    Fix check for repository

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,9 @@
 # Avoid “Another git process seems to be running in this repository” error
 rm -f /home/deno/git-repository/.git/index.lock
 
-# If the folder for the repository exists
-if [ -d "/home/deno/git-repository" ]; then
+# If the repository has been set up
+if [ -d "/home/deno/git-repository/.git" ]
+then
   bash /home/deno/update.sh
 else
   bash /home/deno/setup.sh
