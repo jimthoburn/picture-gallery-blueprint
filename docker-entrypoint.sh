e922e53557664aa12124293c57980c98320a3a6c
Jim Thoburn <jim@tobbi.co>
2024-03-04 20:55:41 -0800

    Make it easier to run setup & build steps using SSH (part 3)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,18 +7,16 @@ rm -f /home/deno/git-repository/.git/index.lock
 if [ -d "/home/deno/git-repository/.git" ]
 then
   echo "Repository already set up. Building..."
-  bash /home/deno/reset.sh
-
+  
   # Run a script, without waiting for it to finish (since waiting for this step may cause the deploy to time out)
   # https://unix.stackexchange.com/questions/86247/what-does-ampersand-mean-at-the-end-of-a-shell-script-line#answer-86253
-  bash /home/deno/build-site.sh &
+  bash /home/deno/reset.sh && bash /home/deno/build-site.sh &
 else
   echo "Setting up repository and building..."
-  bash /home/deno/setup.sh
 
   # Run a script, without waiting for it to finish (since waiting for this step may cause the deploy to time out)
   # https://unix.stackexchange.com/questions/86247/what-does-ampersand-mean-at-the-end-of-a-shell-script-line#answer-86253
-  bash /home/deno/setup-images-and-build.sh &
+  bash /home/deno/setup.sh && bash /home/deno/setup-images-and-build.sh &
 fi
 
 # Hand off to the CMD
