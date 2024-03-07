67a7d861444e27e2c78a96f6b581f4b9a775fbe9
Jim Thoburn <jim@tobbi.co>
2024-03-06 20:57:44 -0800

    Make it easier to run setup & build steps using SSH (part 7)

--- start.sh
+++ start.sh
@@ -7,13 +7,26 @@ rm -f /home/deno/git-repository/.git/index.lock
 if [ -d "/home/deno/git-repository/.git" ]
 then
   echo "Repository already set up. Building..."
+
+  echo "Running reset.sh..."
   bash /home/deno/reset.sh
+
+  echo "Running build-site.sh..."
   bash /home/deno/build-site.sh
+
+  # Start the server
+  echo "Starting the server..."
+  cd /home/deno/git-repository && deno task file-server
 else
   echo "Setting up repository and building..."
-  bash /home/deno/setup.sh
-  bash /home/deno/setup-images-and-build.sh
-fi
 
-# Hand off to the CMD
-cd /home/deno/git-repository && deno task file-server
+  echo "Running setup.sh..."
+  bash /home/deno/setup.sh
+
+  echo "Running setup-images-and-build.sh..."
+  bash /home/deno/setup-images-and-build.sh
+
+  # Start the server
+  echo "Starting the server..."
+  cd /home/deno/git-repository && deno task file-server
+fi
